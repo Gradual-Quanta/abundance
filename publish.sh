159efdfaea0e30e92ddf77a91ec7f1ad747fc51e
Alphonso E. Flegler <aflegler@healthium.gq>
2019-10-26 12:17:15 +0200

    v1.6.11 12:17:12: d12 on 10/26/19 ~ Alphonso F. qm: QmaeVGKEgHJSBFtmwJdorK4RFG2MhVuEMXsvzCDex2ZCTy tag: zY63XDbDXZdpqf9LCQCrGAA7MfomBFhjUC

--- publish.sh
+++ publish.sh
@@ -4,10 +4,10 @@ find . -name "*.*~*" -delete
 jekyll build
 if ! git diff --quiet HEAD^ HEAD; then
 option="--raw-leaves --trickle -s size-1048576 --cid-base=base58btc"
-qm=$(ipfs add -Q -w -r index.md _site/21-days* _data/mantras.yml _data/audio.yml _audios/*.mp3 $option )
-echo http://127.0.0.1:8080/ipfs/$qm
+qm=$(ipfs add -Q -w -r index*.md _site/21-days*/* _data/mantras*.yml _data/audio*.yml _audios/*.mp3 _audios/*.mpeg $option )
+echo http://127.0.0.1:8083/ipfs/$qm
 pm=$(cat _data/ipfs.yml | xyml qm)
-if true; then
+if false; then
 cat <<EOF > _data/ipfs.yml
 --- # blockRing™
 qm: $qm
